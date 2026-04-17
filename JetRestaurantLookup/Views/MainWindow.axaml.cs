04ed5a861206d0c87a9278a75a02280cba475801
David Brett <davidbrett2002@gmail.com>
2026-04-17 19:07:23 +0200

    Initial Avalonia template

---
+++ JetRestaurantLookup/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace JetRestaurantLookup.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
