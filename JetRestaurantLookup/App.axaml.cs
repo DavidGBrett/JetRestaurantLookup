fc4466d4ba91f80919e397fc20b770adf7be876e
David Brett <davidbrett2002@gmail.com>
2026-04-19 17:57:45 +0200

    Changed MainWindowViewModel to receive RestaurantService via injection from App

--- JetRestaurantLookup/App.axaml.cs
+++ JetRestaurantLookup/App.axaml.cs
@@ -4,6 +4,7 @@ using Avalonia.Data.Core;
 using Avalonia.Data.Core.Plugins;
 using System.Linq;
 using Avalonia.Markup.Xaml;
+using JetRestaurantLookup.Core.Services;
 using JetRestaurantLookup.ViewModels;
 using JetRestaurantLookup.Views;
 
@@ -22,7 +23,7 @@ public partial class App : Application
         {
             desktop.MainWindow = new MainWindow
             {
-                DataContext = new MainWindowViewModel(),
+                DataContext = new MainWindowViewModel(new RestaurantService()),
             };
         }
 
