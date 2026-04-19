f2ce27ece536fbc92382cf2b54ce987ac01159e6
David Brett <davidbrett2002@gmail.com>
2026-04-19 18:13:48 +0200

    Removed GetRestaurantsAsync overload using DefaultCount field, and instead use a default parameter value

--- JetRestaurantLookup.Core/Services/IRestaurantService.cs
+++ JetRestaurantLookup.Core/Services/IRestaurantService.cs
@@ -11,13 +11,6 @@ namespace JetRestaurantLookup.Core.Services
         /// <param name="count">The maximum number of restaurants to return.</param>
         /// <exception cref="ArgumentException">Thrown when <paramref name="postcode"/> is null or whitespace.</exception>
         /// <exception cref="ArgumentOutOfRangeException">Thrown when <paramref name="count"/> is zero or negative.</exception>
-        Task<List<Restaurant>> GetRestaurantsAsync(string postcode, int count);
-
-        /// <summary>
-        /// Returns up to <see cref="DefaultCount"/> restaurants in the given postcode.
-        /// </summary>
-        /// <param name="postcode">The UK postcode to search in.</param>
-        /// <exception cref="ArgumentException">Thrown when <paramref name="postcode"/> is null or whitespace.</exception>
-        Task<List<Restaurant>> GetRestaurantsAsync(string postcode);
+        Task<List<Restaurant>> GetRestaurantsAsync(string postcode, int count=10);
     }
 }
