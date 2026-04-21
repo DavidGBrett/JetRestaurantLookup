123fbe027b88076d592fbe020d8e6a5bb9d67363
David Brett <davidbrett2002@gmail.com>
2026-04-21 13:05:08 +0200

    Show number of matches beside each filter checkbox

--- JetRestaurantLookup/ViewModels/CategoryFilterViewModel.cs
+++ JetRestaurantLookup/ViewModels/CategoryFilterViewModel.cs
@@ -5,6 +5,8 @@ namespace JetRestaurantLookup.ViewModels;
 public partial class CategoryFilterViewModel : ObservableObject
 {
     public required string Name { get; init; }
+    public int Count { get; init; }
+    public string DisplayName => $"{Name} ({Count})";
 
     [ObservableProperty]
     public partial bool IsSelected { get; set; }
